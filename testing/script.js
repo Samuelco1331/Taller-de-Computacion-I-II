b38187e36e68fa7178fb24577b7395abe69947bf
Samu <samuraipabon@gmail.com>
2025-05-15 13:08:14 -0400

    practica-final-UAM2PT1

--- testing/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("colorPicker").addEventListener("input", function() {
-    document.getElementById("titulo").style.color = this.value;
-});
-document.getElementById("paddingInput").addEventListener("input", function() {
-    document.getElementById("paddingDiv").style.padding = this.value + "px";
-});
-document.getElementById("marginInput").addEventListener("input", function() {
-    document.getElementById("marginContainer").style.margin = this.value + "px";
-});
